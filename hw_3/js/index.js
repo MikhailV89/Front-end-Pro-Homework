012355f0179d23d01f698d0492044b09d37f8c2d
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-12-15 16:28:05 +0300

    Add files via upload

---
+++ hw_3/js/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const firstValue = +prompt('Write first number?');
+const secondValue = +prompt('Write second number?');
+
+const sum = firstValue + secondValue;
+console.log(sum);
+const minus = firstValue - secondValue;
+console.log(minus);
+const multiply = firstValue * secondValue;
+console.log(multiply);
+const divide = firstValue / secondValue;
+console.log(divide);
+
