bea63cf207bd0247124ad44e1e476c144ab587be
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-12-16 19:18:14 +0300

    Add files via upload

---
+++ hw_2/js/index.js
@@ -0,0 +1,16 @@
+'use strict';
+// Task #1
+
+const Name = 'John';
+const admin = Name;
+alert(admin);
+
+// Task #2
+
+const earthName = 'Earth';
+const siteVisitor = 'some name';
+
+// Task #3
+
+const valueMix = '10' + 10;
+console.log(valueMix);
