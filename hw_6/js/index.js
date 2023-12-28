d19a736efc81de318f3a351011c8286eafa62aaa
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-12-28 14:46:45 +0300

    Add files via upload

---
+++ hw_6/js/index.js
@@ -0,0 +1,26 @@
+'use strict';
+// Створити скрипт для додавання, віднімання, множення та поділу двох чисел та виведення результатів.
+
+// Користувач вводить два числа через два промпти, і повідомлення виводиться алертом з таким результатом:
+
+// //Користувач ввів 2 і 2:
+
+// 2+2=4
+// 2-2=0
+// 2*2=4
+// 2/2=1
+
+const num1 = prompt('Write first number');
+const num2 = prompt('Write second number');
+
+const sum = num1 + num2;
+const minus = num1 - num2;
+const multiply = num1 * num2;
+const divide = num1 / num2;
+
+alert(` Result:
+  Sum ${sum}
+  Minus ${minus}
+  Multiply ${multiply}
+  Divide ${divide}
+`);
