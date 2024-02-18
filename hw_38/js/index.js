13edbf99e60869e2e8e3568ff6ab0207436d744b
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2024-02-18 16:53:15 +0200

    Add files via upload

---
+++ hw_38/js/index.js
@@ -0,0 +1,17 @@
+'use strict';
+//Task 1
+const value = (a, b) => a + b
+
+console.log(value(3, 4));
+
+//Task 2
+
+const strLength = str => str.length;
+
+console.log(strLength('Alice'));
+
+//Task 3
+
+const numbArray = arr => arr.map(num => num + 1);
+
+console.log(numbArray([20, 21, 22]));
